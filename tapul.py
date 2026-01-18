9ea499567aee8dffa3a63de59eb8c06e008e357b
Ikram Al-Baloshi <ikrambaloch112001@gmail.com>
2026-01-19 01:30:58 +0500

    Remove commented-out code and clean up tapul.py

--- tapul.py
+++ tapul.py
@@ -4,16 +4,6 @@ te=("she is a studant")
 print(te[2])
 # baki ka mobail me picture mojood hai dekh lena.
 
-# from google import genai
-
-# client = genai.Client("AIzaSyAOulLsi-UR4p2YAEE7hJcMdBie0K8ghMM")
-
-# response = client.models.generate_content(
-#     model="gemini-2.5-flash",
-#     contents="Explain how AI works in a few words",
-# )
-
-# print(response.text)
 
 
 
