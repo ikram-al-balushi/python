bf6ea3071bc375edd39ab1fb88ce473f0661c89d
Ikram Al-Baloshi <ikrambaloch112001@gmail.com>
2026-01-19 01:31:54 +0500

    Update tapul.py

--- tapul.py
+++ tapul.py
@@ -2,8 +2,3 @@
 # # ar [] ye alamat hain liste k ye srif coount ar indexs maloom krne k lie istmal hoga.
 te=("she is a studant")
 print(te[2])
-# baki ka mobail me picture mojood hai dekh lena.
-
-
-
-
