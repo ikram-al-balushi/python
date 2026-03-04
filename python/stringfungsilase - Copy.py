19edb48f1672fab02ba009032c7bcc9db4a378d8
Your Name <your.email@example.com>
2026-03-04 16:28:41 +0500

    Add practice file

---
+++ python/stringfungsilase - Copy.py
@@ -0,0 +1,20 @@
+
+a="hello"
+print(len(a))
+a="HELLO"
+print(a.lower())
+a="hello"
+print(a.upper())
+a="paython is fine ikram"
+print(a.capitalize())
+a="i love java"
+print(a.replace("java","ikram"))
+a="ikram khanani"
+print(a.find("k"))
+a=("babakaka")
+print(a.count("b"))
+a="ikramalbaloshi"
+print(a[0:3])
+
+
+
